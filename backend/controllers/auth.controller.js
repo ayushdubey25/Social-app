322591d5c3f57c8cae3cd96b4f7e1e208164f428
Ayush Dubey <154453637+ayushdubey25@users.noreply.github.com>
2024-12-10 12:29:30 +0530

    1.user routes and controllers

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,15 +1,109 @@
+import User from "../models/user.model.js";
+import { generateTokenAndSetCookies } from "../lib/utils/generateToken.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-  res.json({
-    data: "You hit the signup point",
-  });
+  try {
+    const { fullname, username, email, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "Invalid email format" });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ error: "Email is already taken" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: "Password must be at least 6 characters long" });
+    }
+    // hash password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newuser = new User({
+      fullname,
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newuser) {
+      generateTokenAndSetCookies(newuser._id, res);
+      await newuser.save();
+      res.status(201).json({
+        _id: newuser._id,
+        fullname: newuser.fullname,
+        username: newuser.username,
+        email: newuser.email,
+        followers: newuser.followers,
+        following: newuser.following,
+        profileImg: newuser.profileImg,
+        coverImg: newuser.coverImg,
+      });
+    } else {
+      res.status(400).json({ error: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ error: "Internal Server error" });
+  }
 };
 export const login = async (req, res) => {
-  res.json({
-    data: "You hit the login point",
-  });
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndSetCookies(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullname: user.fullname,
+      username: user.username,
+      email: user.email,
+      follower: user.follower,
+      following: user.following,
+      profileImg: user.profileImg,
+      coverImgg: user.coverImg,
+    });
+  } catch (error) {
+    console.log("Error in login controller", error.message);
+    res.status(500).json({ error: "Internal Server error" });
+  }
 };
 export const logout = async (req, res) => {
-  res.json({
-    data: "You hit the logout point",
-  });
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log("Error in getMe controller", error.message);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,34 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import dotenv from "dotenv";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: true })); //to parse form data(urlencoded)
+
 console.log(process.env.MONGO_URI);
 
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT} `);
