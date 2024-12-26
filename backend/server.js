8c876a219130834105d3a1e94b80659c875280a4
Ayush Dubey <154453637+ayushdubey25@users.noreply.github.com>
2024-12-26 11:12:45 +0530

    3.Suggested users fetched

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ cloudinary.config({
 });
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json({ limit: "5mb" })); //to parse req.body
 //limit shouldn't be too large to protect DOS attack
